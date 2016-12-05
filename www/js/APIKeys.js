517431e019137e56683ae35dffd8366c99e9bb03
Daniel Naranjo <dnaranjo@gmail.com>
2016-12-05 12:00:13 -0300

    .

--- www/js/APIKeys.js
+++ www/js/APIKeys.js
@@ -7,11 +7,11 @@
 // API KEYS FOR INDOORATLAS SERVICES AND GOOGLE MAPS JAVASCRIPT API
 
 // Get Google Maps API Key from here: https://console.developers.google.com/apis/credentials
-var GOOGLE_API_KEY = '';
+var GOOGLE_API_KEY = 'AIzaSyCk9_aX-0E1C0f-cF29fmBKnGqvVp5BHRo';
 
 // Get IndoorAtlas API Key and Secret from here https://app.indooratlas.com/apps
-var IA_API_KEY = '';
-var IA_API_SECRET = '';
+var IA_API_KEY = '31070d9c-689d-4bd9-ae0b-05af49ec2b56';
+var IA_API_SECRET = 'hJEBk1N5QUperpTrvIO20CeMkUU9ndeHSoYnW2GW0Zhp65JZfj5hAwRhowSleJ3CPYS3mkY80wSi6DamyaYvXFisyBYzVQ52/gRccOE6keGCpw3OvpQXZ9FNRqCSvA==';
 
 // Floorplan ID https://developer.indooratlas.com/venues
 var IA_FLOORPLAN_ID = '';
